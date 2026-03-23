143400f4aaa2895c83d9200537243265c9f2c68c
Marcell Parra <parra.silva@ime.eb.br>
2026-03-23 10:31:41 -0300

    Widget4

--- src/app/widget4/widget4.ts
+++ src/app/widget4/widget4.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { User } from '../app';
 
 @Component({
@@ -7,6 +8,33 @@ import { User } from '../app';
   templateUrl: './widget4.html',
   styleUrl: './widget4.css',
 })
-export class Widget4 {
+export class Widget4 implements OnInit {
   @Input() user?: User;
+  private http = inject(HttpClient);
+
+  fact: string = '';
+  loading = signal(false)
+  error: string = '';
+
+  ngOnInit() {
+    this.fetchFact();
+  }
+
+  fetchFact() {
+    this.loading.set(true);
+    this.error = '';
+    this.fact = '';
+
+    this.http.get<any>('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en')
+      .subscribe({
+        next: (response) => {
+          this.fact = response.text;
+          this.loading.set(false);
+        },
+        error: (err) => {
+          this.error = 'Erro ao carregar fato.';
+          this.loading.set(false);
+        }
+      });
+  }
 }
