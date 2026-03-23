614093e29287c4bd96c8d340fbf07ce4ceb1aa6e
Álisson Santana <45105953+alisson94@users.noreply.github.com>
2026-03-23 09:55:03 -0300

    Add widget1: slip advices

--- src/app/widget1/widget1.ts
+++ src/app/widget1/widget1.ts
@@ -1,12 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { User } from '../app';
+import { CommonModule } from '@angular/common';
+
+type SlipResponse = {
+  slip?: {
+    id?: number;
+    advice?: string;
+  };
+};
 
 @Component({
   selector: 'app-widget1',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './widget1.html',
   styleUrl: './widget1.css',
 })
-export class Widget1 {
+export class Widget1{
   @Input() user?: User;
+
+  data = signal<SlipResponse | null>(null);
+  loading = signal<boolean>(false);
+  error = signal<string | null>(null)
+
+  async fetchData(): Promise<void> {
+    this.loading.set(true);
+    this.error.set(null);
+
+    try{
+      const response = await fetch('https://api.adviceslip.com/advice');
+      if(!response.ok){
+        throw new Error(`Erro: ${response.status}`);
+      }
+      const dataJson = (await response.json()) as SlipResponse;
+
+      this.data.set(dataJson);
+
+      console.log(this.data()?.slip?.advice);
+      console.log(this.data()?.slip?.id);
+
+    } catch (error: any) {
+      this.error.set(error.message);
+      console.error('Erro:', error);
+    } finally {
+      this.loading.set(false);
+    }
+  }
+
 }
